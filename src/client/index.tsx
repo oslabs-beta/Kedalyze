0b32c3db0e58d5247262cb1d90277a66d860d99c
ks1009 <karenshi1009@gmail.com>
2022-11-22 13:44:53 -0500

    changed server.js to server.ts

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
-const container = document.getElementById('root');
+const container = document.getElementById('root') as HTMLInputElement | null;
 const root = createRoot(container);
 
 root.render(
