701f0581e3d15ccc6aa76eb9ec447cbc74d0dad0
Debbie Zavaleta <d.zavaleta96@gmail.com>
2022-11-23 16:52:04 -0500

    server side now functional with command npm run server

--- src/server/metrics.ts
+++ src/server/metrics.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import client from 'prom-client'
+// import express from 'express';
+// import client from 'prom-client'
 
-const app = express();
+// const app = express();
 
-export function startMetricsServer(){
-    const collectDefaultMetrics = client.collectDefaultMetrics;
-    collectDefaultMetrics();
+// export function startMetricsServer(){
+//     const collectDefaultMetrics = client.collectDefaultMetrics;
+//     collectDefaultMetrics();
 
-    app.get('/metrics', async (req, res) => {
-        res.set('Content-Type', client.register.contentType)
+//     app.get('/metrics', async (req, res) => {
+//         res.set('Content-Type', client.register.contentType)
 
-        return res.send(await client.register.metrics)
-    })
+//         return res.send(await client.register.metrics)
+//     })
 
 
-    app.listen(9100, () => {
-        console.log('Metrics server started at http://localhost:9100')
-    })
+//     app.listen(9100, () => {
+//         console.log('Metrics server started at http://localhost:9100')
+//     })
 
-}
+// }
